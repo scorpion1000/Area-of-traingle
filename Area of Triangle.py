9e880608a7ffc316d75f3bdabb7f444a146f4472
scorpion1000 <67224809+scorpion1000@users.noreply.github.com>
2020-06-21 17:36:16 +0500

    Add files via upload

---
+++ Area of Triangle.py
@@ -0,0 +1,17 @@
+## 8. Triangle area
+### Write a Python program that will accept the base and height of a triangle and compute the area
+
+print('Area of a Triangle')
+
+# Getting User Input
+
+base = float(input('Base : '))
+  
+height = float(input('Height : '))
+
+ # Now getting into Formulae
+Area = (base * height) / 2
+
+ #Output
+
+print(f'Area {Area} with base {base} & Height {height} ')
